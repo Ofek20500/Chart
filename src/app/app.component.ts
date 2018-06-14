67ad07357953ead5bc2716e4eb39a9781de6b9dd
Ofek20500 <40270046+Ofek20500@users.noreply.github.com>
2018-06-14 18:20:20 +0300

    add to try

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,42 @@ import { VarService } from './var.service';
 export class AppComponent implements OnInit {
   title = 'app';
 
+  actweight:number;
+  aircraft:string = '';
+  cbm:number;
+  chargeweight:number;
+  etd:string;
+  flightno:string;
+  fport:string;
+  id:number;
+  income:number;
+  numofpieces:number;
+  tport:string;
 
-constructor() {}
+
+constructor(private httpClient:HttpClient) {}
+
+onNameKeyUp(event:any){
+  console.log(event.target.value);
+  this.aircraft = event.target.value;
+  console.log(this.aircraft = event.target.value);
+}
 
 ngOnInit() {
   
 }
 
+getProfile(){
+  this.httpClient.get(`https://my-json-server.typicode.com/Ofek20500/Chart/list`)
+  .subscribe(
+    (data:any[]) => {
+      // if(data.length) {
+      //   this.cbm = data[0].cbm;
+      // }
+      console.log(data[0]);
+    }
+  )
+  console.log(this.aircraft);
+}
 
 }
